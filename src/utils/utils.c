f52bcce8a1154d6d6d86be01451386b2b6ac7ee8
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-05-24 22:37:34 +0200

    add normalize function

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -5,3 +5,7 @@ float clamp(float val, float inf, float sup) {
   float v = val < inf ? inf : val;
   return v > sup ? sup : v;
 }
+
+float normalize(float val, float min, float max) {
+  return (val - min) / (max - min);
+}
